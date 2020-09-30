0cfc5be6f523906eb1542fda6e1215d1954e63cf
WickednessPro <wickednessproductions@gmail.com>
2020-09-30 18:49:00 +1000

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
-
-const PORT = process.env.PORT || 8080
-app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
-
-app.use('/', require('./Main'));
